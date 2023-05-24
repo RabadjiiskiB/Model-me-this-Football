4d1588a469e90661994ded2850c65d709d4ad505
RabadjiiskiB <76592514+RabadjiiskiB@users.noreply.github.com>
2023-05-24 10:24:48 +0300

    Add files via upload

---
+++ Football/Football/Game.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Football
+{
+    internal class Game
+    {
+        private List<Player> team1;
+        private List<Player> team2;
+        private Referee referee;
+        private Referee assistant_referee;
+        private Referee assistant_referee2;
+        private List<Goals> goals;
+        private string result;
+        private Team winner;
+        public Game(List<Player> team1, List<Player> team2, Referee referee, Referee assistant_referee, Referee assistant_referee2, List<Goals> goals, string result, Team winner)
+        {
+            while(team1.Count<=11)
+            {
+                this.team1.Add(team1[0]);
+            }
+            while(team2.Count<=11)
+            {
+                this.team2.Add(team2[0]);
+            }
+
+            this.referee = referee;
+            this.assistant_referee = assistant_referee;
+            this.assistant_referee2 = assistant_referee2;
+            this.goals = goals;
+            this.result = result;
+            this.winner = winner;
+        }
+    }
+}
---
+++ Football/Football/Team.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Football
+{
+    internal class Team
+    {
+        private string coach;
+        private List<Player> players;
+        private int age;
+        public Team(string coach, List<Player> players)
+        {
+            if(players.Count>=11 && players.Count <= 22)
+            {
+                this.coach = coach;
+                this.players = players;
+                int sum = 0;
+                foreach (Player player in players)
+                {
+                    sum += player.Age;
+                }
+                this.age = sum / players.Count;
+            }
+            else if(players.Count <11)
+            {
+                Console.WriteLine("Player count is not sufficient");
+            }
+            else if (players.Count > 22)
+            {
+                Console.WriteLine("Too many players");
+            }
+        }
+    }
+}
